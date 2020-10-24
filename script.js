f945a157ef58d57098de19f33aa14d78b437ea84
Felipe Guerreiro <f.guerreiro@live.com>
2020-10-24 19:27:39 -0300

    Make the arms move
    - Made function for actually moving the arms non stop

--- script.js
+++ script.js
@@ -2,20 +2,27 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-//set arms position
-let hrPosition = 20;
-let minPosition = 140;
-let secPosition = 398;
+//run clock function
+function runClock() {
+    //get current date
+    var date = new Date();
+    console.log(date);
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    console.log("Hour: "+hr+ " Minute: "+min+ " Second: " +sec);
 
-//get current date
-var date = new Date();
-console.log(date);
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-console.log("Hour: "+hr+ " Minute: "+min+ " Second: " +sec);
+    //set arms position
+    let hrPosition = (hr*360/12)+(min*(360/60)/12);
+    let minPosition = (min*360/60)+(sec*(360/60)/60);
+    let secPosition = sec*360/60;
 
-//rotate the clock arms
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+    //rotate the clock arms
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+}
+
+//set runClock function to run every 1000 milisecs usgin setInterval()
+var interval = setInterval(runClock, 1000);
