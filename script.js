6f251416a2373e5a1b800b6cdf2045eccbd9b517
Felipe Guerreiro <f.guerreiro@live.com>
2020-10-24 19:07:58 -0300

    Get current date
    - Add lines to get current date using Date()

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+//set arms position
 let hrPosition = 20;
 let minPosition = 140;
 let secPosition = 398;
 
+//get current date
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: "+hr+ " Minute: "+min+ " Second: " +sec);
+
+//rotate the clock arms
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
 SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
